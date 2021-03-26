b30cf863938ed136dfb35c25f107f9466f32323d
gary <Gary_Grumbley@intuit.com>
2021-03-26 13:51:26 -0700

    fix: github auth missing fields

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,8 @@
-
-import { NextApiHandler } from 'next'
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
-import Adapters from 'next-auth/adapters'
-import prisma from '../../../lib/prisma'
-
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
-export default authHandler
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import prisma from '../../../lib/prisma';
 
 const options = {
   providers: [
@@ -17,4 +13,8 @@ const options = {
   ],
   adapter: Adapters.Prisma.Adapter({ prisma }),
   secret: process.env.SECRET,
-}
+};
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+
+export default authHandler;
