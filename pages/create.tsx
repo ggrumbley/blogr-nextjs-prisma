e589e1c86246686300e10b907a250cff7c1485c0
Gary Grumbley <ggrumbley@gmail.com>
2021-03-29 08:24:19 -0700

    chore: scaffold add post

---
+++ pages/create.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import Router from 'next/router';
+
+import Layout from '../components/Layout';
+import { $fetch } from '../lib/bling';
+const Draft: React.FC = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const submitData = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    try {
+      await $fetch.post('/api/post', { title, content });
+      await Router.push('/drafts');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Layout>
+      <div>
+        <form onSubmit={submitData}>
+          <h1>New Draft</h1>
+          <input
+            autoFocus
+            placeholder="Title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Content"
+            cols={50}
+            rows={8}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
+          <input disabled={!content || !title} type="submit" value="Create" />
+          <a className="back" href="#" onClick={() => Router.push('/')}>
+            or Cancel
+          </a>
+        </form>
+      </div>
+      <style jsx>{`
+        .page {
+          background: white;
+          padding: 3rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        input[type='text'],
+        textarea {
+          width: 100%;
+          padding: 0.5rem;
+          margin: 0.5rem 0;
+          border-radius: 0.25rem;
+          border: 0.125rem solid rgba(0, 0, 0, 0.2);
+        }
+
+        input[type='submit'] {
+          background: #ececec;
+          border: 0;
+          padding: 1rem 2rem;
+        }
+
+        .back {
+          margin-left: 1rem;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default Draft;
